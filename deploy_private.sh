0c3a6ed84cd18be667934d2d694bd916ca7ff344
KimUnBi <kwb5025@gmail.com>
2025-09-10 11:22:45 +0900

    final commit

--- deploy_private.sh
+++ deploy_private.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Private 서버 정보 ---
-PRIVATE_HOST="10.10.20.6"          # 실제 Private 서버 IP
+PRIVATE_HOST="10.10.20.7"          # 실제 Private 서버 IP
 PRIVATE_USER="root"                 # Private 서버 계정
 PRIVATE_KEY="/root/.ssh/id_rsa"     # Public 서버에서 Private 서버 접속용 키
 
@@ -17,7 +17,7 @@ ssh-add $PRIVATE_KEY
 ssh -o StrictHostKeyChecking=no $PRIVATE_USER@$PRIVATE_HOST "mkdir -p /root/app"
 
 
-scp -o StrictHostKeyChecking=no /root/app/*.jar $PRIVATE_USER@$PRIVATE_HOST:/root/app/app.jar
+scp -o StrictHostKeyChecking=no /root/app/build/libs/*.jar $PRIVATE_USER@$PRIVATE_HOST:/root/app/app.jar
 
 
 ssh -o StrictHostKeyChecking=no $PRIVATE_USER@$PRIVATE_HOST "nohup java -jar /root/app/app.jar > /root/app/app.log 2>&1 &"
